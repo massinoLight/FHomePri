d252e2212369fc264c9ce21cd087637aecdd2bc0
massino <nacerddine.amassin@gmail.com>
2020-04-10 16:42:05 +0200

    commit de test

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -4,6 +4,24 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import LoginView from '../Components/LoginView'
+
+
+const ApplicationNavigator = createStackNavigator({
+  LoginView: {
+    screen: LoginView,
+    navigationOptions: {
+      title: 'Login'
+    }
+  }
+})
+export default createAppContainer(ApplicationNavigator)
+
+
+
+
+
+
 
 function HomeScreen() {
   return (
